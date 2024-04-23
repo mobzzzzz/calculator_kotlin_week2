e9aaf5e207b6b93de593f7bafca86ab58b38807e
Jaewon Noh <opqw987@gmail.com>
2024-04-23 21:06:35 +0900

    Level 1, 2 수정

--- src/Level1.kt
+++ src/Level1.kt
@@ -1,21 +1,4 @@
-fun main() {
-    val calculator = Level1.Calculator()
-
-    println("숫자 두개 입력")
-    val num1 = readln().toInt()
-    val num2 = readln().toInt()
-    println("연산자 입력 (+, -, *, /)")
-    val operator = readln()
-
-    when (operator) {
-        "+" -> println(calculator.add(num1, num2))
-        "-" -> println(calculator.subtract(num1, num2))
-        "*" -> println(calculator.multiply(num1, num2))
-        "/" -> println(calculator.divide(num1, num2))
-    }
-}
-
-class Level1 {
+class Level1 { // 이건 껍데기임
     class Calculator {
         fun add(a: Int, b: Int): Int {
             return a + b
@@ -34,3 +17,35 @@ class Level1 {
         }
     }
 }
+
+fun main() {
+    println("첫번째 숫자 입력")
+    val num1 = readln().toInt()
+    println("두번째 숫자 입력")
+    val num2 = readln().toInt()
+    println("연산자 입력 (+, -, *, /)")
+    val operator = readln()
+
+    println(
+        "$num1 $operator $num2 = ${
+            when (operator) {
+                "+" -> num1 + num2
+                "-" -> num1 - num2
+                "*" -> num1 * num2
+                "/" -> num1 / num2
+                else -> "에러입니다"
+            }
+        }"
+    )
+
+    val calculator = Level1.Calculator()
+
+    println(
+        if (operator == "+") calculator.add(num1, num2)
+        else if (operator == "-") calculator.subtract(num1, num2)
+        else if (operator == "*") calculator.multiply(num1, num2)
+        else if (operator == "/") calculator.divide(num1, num2)
+        else "에러입니다"
+    )
+}
+
--- src/Level2.kt
+++ src/Level2.kt
@@ -1,36 +1,86 @@
 fun main() {
-    val calculator = Level2.Calculator()
-
-    println("숫자 두개 입력")
+    println("첫번째 숫자 입력")
     val num1 = readln().toInt()
+    println("두번째 숫자 입력")
     val num2 = readln().toInt()
-    println("연산자 입력 (+, -, *, /)")
+    println("연산자 입력 (+, -, *, /, %)")
     val operator = readln()
 
-    when (operator) {
-        "+" -> println(calculator.add(num1, num2))
-        "-" -> println(calculator.subtract(num1, num2))
-        "*" -> println(calculator.multiply(num1, num2))
-        "/" -> println(calculator.divide(num1, num2))
-    }
+    Level2.Calculator(num1, num2, operator)
 }
 
 class Level2 {
     class Calculator {
-        fun add(a: Int, b: Int): Int {
-            return a + b
+        var num1: Int = 0
+        var num2: Int = 0
+        var operator: String = ""
+
+        constructor(num1: Int, num2: Int, operator: String) {
+            this.num1 = num1
+            this.num2 = num2
+            this.operator = operator
+
+            when(operator) {
+                "+" -> addResult()
+                "-" -> subtractResult()
+                "*" -> multiplyResult()
+                "/" -> divideResult()
+                "%" -> modularResult()
+            }
         }
 
-        fun subtract(a: Int, b: Int): Int {
-            return a - b
+        fun addResult() {
+            println("$num1 $operator $num2 = ${num1 + num2}")
         }
 
-        fun multiply(a: Int, b: Int): Int {
-            return a * b
+        fun subtractResult() {
+            println("$num1 $operator $num2 = ${num1 - num2}")
         }
 
-        fun divide(a: Int, b: Int): Int {
-            return a / b
+        fun multiplyResult() {
+            println("$num1 $operator $num2 = ${num1 * num2}")
+        }
+
+        fun divideResult() {
+            println("$num1 $operator $num2 = ${num1 / num2}")
+        }
+
+        fun modularResult() {
+            println("$num1 $operator $num2 = ${num1 % num2}")
+        }
+
+
+
+
+        fun add(): Int {
+            return num1 + num2
+        }
+
+        fun subtract(): Int {
+            return num1 - num2
+        }
+
+        fun multiply(): Int {
+            return num1 * num2
+        }
+
+        fun divide(): Int {
+            return num1 / num2
+        }
+
+        fun modular(): Int {
+            return num1 % num2
+        }
+
+        fun operate(): Int {
+            return when (operator) {
+                "+" -> add()
+                "-" -> subtract()
+                "*" -> multiply()
+                "/" -> divide()
+                "%" -> modular()
+                else -> 0
+            }
         }
     }
 }
