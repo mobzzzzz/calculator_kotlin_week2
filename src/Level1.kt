c7d9e2f59a116b47791315f207b2bfc7852f24e0
Jaewon Noh <opqw987@gmail.com>
2024-04-24 11:11:59 +0900

    Update Level1.kt

--- src/Level1.kt
+++ src/Level1.kt
@@ -1,4 +1,35 @@
-class Level1 { // 이건 껍데기임
+fun main() {
+    println("첫번째 숫자 입력")
+    val num1 = readln().toInt()
+    println("두번째 숫자 입력")
+    val num2 = readln().toInt()
+    println("연산자 입력 (+, -, *, /)")
+    val operator = readln()
+
+//    println(
+//        "$num1 $operator $num2 = ${
+//            when (operator) {
+//                "+" -> num1 + num2
+//                "-" -> num1 - num2
+//                "*" -> num1 * num2
+//                "/" -> num1 / num2
+//                else -> "에러입니다"
+//            }
+//        }"
+//    )
+
+    val calculator = Level1.Calculator()
+
+    println(
+        if (operator == "+") calculator.add(num1, num2)
+        else if (operator == "-") calculator.subtract(num1, num2)
+        else if (operator == "*") calculator.multiply(num1, num2)
+        else if (operator == "/") calculator.divide(num1, num2)
+        else "에러입니다"
+    )
+}
+
+class Level1 {
     class Calculator {
         fun add(a: Int, b: Int): Int {
             return a + b
@@ -17,35 +48,3 @@ class Level1 { // 이건 껍데기임
         }
     }
 }
-
-fun main() {
-    println("첫번째 숫자 입력")
-    val num1 = readln().toInt()
-    println("두번째 숫자 입력")
-    val num2 = readln().toInt()
-    println("연산자 입력 (+, -, *, /)")
-    val operator = readln()
-
-    println(
-        "$num1 $operator $num2 = ${
-            when (operator) {
-                "+" -> num1 + num2
-                "-" -> num1 - num2
-                "*" -> num1 * num2
-                "/" -> num1 / num2
-                else -> "에러입니다"
-            }
-        }"
-    )
-
-    val calculator = Level1.Calculator()
-
-    println(
-        if (operator == "+") calculator.add(num1, num2)
-        else if (operator == "-") calculator.subtract(num1, num2)
-        else if (operator == "*") calculator.multiply(num1, num2)
-        else if (operator == "/") calculator.divide(num1, num2)
-        else "에러입니다"
-    )
-}
-
