12f20c70f660b589969ef1e36d587b42314e5e1d
Jaewon Noh <opqw987@gmail.com>
2024-04-24 11:02:13 +0900

    Update Level2.kt

--- src/Level2.kt
+++ src/Level2.kt
@@ -1,86 +1,51 @@
 fun main() {
-    println("첫번째 숫자 입력")
-    val num1 = readln().toInt()
-    println("두번째 숫자 입력")
-    val num2 = readln().toInt()
-    println("연산자 입력 (+, -, *, /, %)")
+
+    val calculator = Level2.Calculator()
+
+
+    println("무슨 계산을 하고 싶으세요? 현재 숫자1: ${calculator.num1} 현재 숫자2: ${calculator.num2}")
+    println("(+, -, *, /, %)")
     val operator = readln()
 
-    Level2.Calculator(num1, num2, operator)
+    when (operator) {
+        "+" -> calculator.showAddResult()
+        "-" -> calculator.showSubsctractResult()
+        "*" -> calculator.showMultiplyResult()
+        "/" -> calculator.showDivideResult()
+        "%" -> calculator.showModularResult()
+    }
 }
 
 class Level2 {
     class Calculator {
-        var num1: Int = 0
-        var num2: Int = 0
-        var operator: String = ""
+        var num1: Int
+        var num2: Int
 
-        constructor(num1: Int, num2: Int, operator: String) {
-            this.num1 = num1
-            this.num2 = num2
-            this.operator = operator
-
-            when(operator) {
-                "+" -> addResult()
-                "-" -> subtractResult()
-                "*" -> multiplyResult()
-                "/" -> divideResult()
-                "%" -> modularResult()
-            }
+        init {
+            println("계산기에 저장할 첫번째 숫자 입력")
+            num1 = readln().toInt()
+            println("계산기에 저장할 두번째 숫자 입력")
+            num2 = readln().toInt()
         }
 
-        fun addResult() {
-            println("$num1 $operator $num2 = ${num1 + num2}")
+        fun showAddResult() {
+            println("$num1 + $num2 = ${num1 + num2}")
         }
 
-        fun subtractResult() {
-            println("$num1 $operator $num2 = ${num1 - num2}")
+        fun showSubsctractResult() {
+            println("$num1 - $num2 = ${num1 - num2}")
         }
 
-        fun multiplyResult() {
-            println("$num1 $operator $num2 = ${num1 * num2}")
+        fun showMultiplyResult() {
+            println("$num1 * $num2 = ${num1 * num2}")
         }
 
-        fun divideResult() {
-            println("$num1 $operator $num2 = ${num1 / num2}")
+        fun showDivideResult() {
+            println("$num1 / $num2 = ${num1 / num2}")
         }
 
-        fun modularResult() {
-            println("$num1 $operator $num2 = ${num1 % num2}")
-        }
-
-
-
-
-        fun add(): Int {
-            return num1 + num2
-        }
-
-        fun subtract(): Int {
-            return num1 - num2
-        }
-
-        fun multiply(): Int {
-            return num1 * num2
-        }
-
-        fun divide(): Int {
-            return num1 / num2
-        }
-
-        fun modular(): Int {
-            return num1 % num2
-        }
-
-        fun operate(): Int {
-            return when (operator) {
-                "+" -> add()
-                "-" -> subtract()
-                "*" -> multiply()
-                "/" -> divide()
-                "%" -> modular()
-                else -> 0
-            }
+        fun showModularResult() {
+            println("$num1 % $num2 = ${num1 % num2}")
         }
     }
 }
