29e44a3a0ffd725bc52532101d1c7b313acbd5d7
Jaewon Noh <opqw987@gmail.com>
2024-04-25 11:58:58 +0900

    Level4 Expansion 문자열 처리 연산 추가

--- src/Level4Expansion.kt
+++ src/Level4Expansion.kt
@@ -1,9 +1,12 @@
 fun main() {
     try {
         Level4Expansion.Calculator()
-    } catch (e: Throwable) { e.printStackTrace() }
+    } catch (e: Throwable) {
+        e.printStackTrace()
+    }
 }
 
+// 문자열 연산 처리 (전위 표기나 후위 표기법 계산은 나중에)
 class Level4Expansion {
     class Calculator {
         init {
@@ -12,60 +15,44 @@ class Level4Expansion {
 
         private fun initCalculator() {
             println("계산기입니다.")
-            println("계산 식을 입력해주세요.")
+            println("계산 식을 입력해주세요. (괄호 및 사칙연산 우선순위 기능은 제공되지 않습니다.)")
             val input = readln()
 
             // 숫자 저장소 (연산 처리 전후 전부)
-            val numbersInResult = mutableListOf<Double>()
-            Regex("""\d+|\S""").findAll(input).forEach { result ->
-                // 숫자면 저장소에 담고 return
-                result.value.toDoubleOrNull()?.let { numbersInResult.add(it); return }
+            var result = 0.0
+            Regex("""\d+|\S""").findAll(input).toList().map { it.value }.let { matchResults ->
 
-                val firstNum = numbersInResult.removeLast()
-                val secondNum = numbersInResult.removeLast()
-                val operator = result.value
+                // Guard clause
+                if (matchResults.count() <= 3) {
+                    throw Exception("수가 적어 연산이 불가능합니다.")
+                }
+                if (!matchResults.last().single().isDigit()) {
+                    throw Exception("수식이 완성되지 않았습니다.")
+                }
 
-                numbersInResult.add(when (operator) {
-                    "+" -> firstNum + secondNum
-                    "-" -> firstNum - secondNum
-                    "*" -> firstNum * secondNum
-                    "/" -> firstNum / secondNum
-                    "%" -> firstNum % secondNum
-                    else -> throw IllegalArgumentException("${operator}는 틀린 연산자입니다.")
-                })
+                result = matchResults.first().toDouble()
+
+                matchResults.forEachIndexed { index, value ->
+                    // 피연산자는 패스
+                    if (index % 2 == 0) {
+                        return@forEachIndexed
+                    }
+
+                    val firstNum = result
+                    val secondNum = matchResults[index + 1].toDouble()
+                    result = when (value) {
+                        "+" -> firstNum + secondNum
+                        "-" -> firstNum - secondNum
+                        "*" -> firstNum * secondNum
+                        "/" -> firstNum / secondNum
+                        "%" -> firstNum % secondNum
+                        else -> throw IllegalArgumentException("${value}는 틀린 연산자입니다.")
+                    }
+
+                }
             }
 
-            println(numbersInResult.removeLast())
-        }
-
-        interface Operation {
-            fun operate(num1: Double, num2: Double): Double
-        }
-
-        class AddOperation: Operation {
-            override fun operate(num1: Double, num2: Double): Double = num1 + num2
-        }
-
-        class SubtractOperation: Operation {
-            override fun operate(num1: Double, num2: Double): Double = (num1 - num2)
-        }
-
-        class MultiplyOperation: Operation {
-            override fun operate(num1: Double, num2: Double): Double = (num1 * num2)
-        }
-
-        class DivideOperation: Operation {
-            override fun operate(num1: Double, num2: Double): Double {
-                if (num2 == 0.0) throw IllegalArgumentException("Division by zero")
-                return num1 / num2
-            }
-        }
-
-        class ModuloOperation: Operation {
-            override fun operate(num1: Double, num2: Double): Double {
-                if (num2 == 0.0) throw IllegalArgumentException("Modulo by zero")
-                return num1 % num2
-            }
+            println("$input 계산 결과는 $result 입니다.")
         }
     }
 }
