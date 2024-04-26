7011116b80dee889e7084795e3b5e1a23b6307af
Jaewon Noh <opqw987@gmail.com>
2024-04-26 11:56:24 +0900

    Docs: 테스트용 강의 파일 정리

--- src/Lesson_week3.kt
+++ src/Lesson_week3.kt
@@ -1,3 +1,33 @@
+
 fun main() {
+    var bird = Bird("새")
+    var chicken = Chicken("닭")
+    var sparrow = Sparrow("참새")
+    var pigeon = Pigeon("비둘기")
+
+    bird.fly()
+    chicken.fly()
+    sparrow.fly()
+    pigeon.fly()
+}
+
+open class Bird(
+    var name: String
+) {
+    fun fly() {
+        println("${name} 날아요~")
+    }
+
+}
+
+class Chicken(name: String) : Bird(name) {
+
+}
+
+class Sparrow(name: String) : Bird(name) {
+
+}
+
+class Pigeon(name: String) : Bird(name) {
 
 }
