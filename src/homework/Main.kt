991950fa46806af68c80752e122ede6055e912ef
Jaewon Noh <opqw987@gmail.com>
2024-05-01 16:41:36 +0900

    Feature: Lions class 예시 추가

--- src/homework/Main.kt
+++ src/homework/Main.kt
@@ -1,16 +1,36 @@
 package homework
 
 fun main() {
-    val calc = Calculator()
-    println("1 더하기 2 결과는 : ${calc.addOperation(AddOperation(), 1, 2)} 입니다")
+    lions()
 }
 
-class AddOperation{
-    fun operate(num1: Int, num2: Int): Double = (num1 + num2).toDouble()
+fun lions() {
+    var lion = Lion(1000, 4, "brown", 300)
+    var lionRed = Lion(1000, 4, "red", 300)
+    var defaultLion = Lion()
+
+    info(lion)
+    info(lionRed)
+    info(defaultLion)
 }
 
-class Calculator {
-    fun addOperation(##답안##): Double {
-        return ##답안##
-    }
+fun info(lion: Lion) {
+    println(lion.furCount)
+    println(lion.legCount)
+    println(lion.color)
+    println(lion.weight)
 }
+
+class Lion(var furCount: Int,
+           var legCount: Int,
+           var color: String,
+           var weight: Int) {
+    constructor() : this(0, 0, "default", 0)
+}
+
+
+// 질럿, 저글링, 마린
+//체력 공격력 방어력
+//걷기 공격하기 공격받기
+//생성자로 체공방 조절하기 (영웅유닛처럼..)
+// 매개변수 유무, 반환 유무로 4가지 행위 모두 만들기
